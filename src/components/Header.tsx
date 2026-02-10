54188dd48c4eae0c54608c2023a405214bdd5d72
GiraFFik <a.zinger2003@gmail.com>
2026-02-10 23:52:34 +0500

    fix logo again

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
+import logo from '../public/logo.png'
+
 export default function Header() {
     return(
         <div className="header">
             <div className="container">
                 <div className="header__inner">
-                    <img className="header__logo" src="../../public/logo.png" alt="logo" />
+                    <img className="header__logo" src={logo} alt="logo" />
                 </div>
             </div>
         </div>
